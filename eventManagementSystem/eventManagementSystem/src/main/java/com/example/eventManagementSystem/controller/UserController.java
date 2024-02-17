7795c58868a9ef1a5ee242c1825f5acc40ca5c18
thalapathy <visweswararaja.r.2018.cse@ritchennai.edu.in>
2024-02-17 12:37:27 +0530

    User

--- eventManagementSystem/eventManagementSystem/src/main/java/com/example/eventManagementSystem/controller/UserController.java
+++ eventManagementSystem/eventManagementSystem/src/main/java/com/example/eventManagementSystem/controller/UserController.java
@@ -1,44 +1,44 @@
-//package com.example.eventManagementSystem.controller;
-//
-//import com.example.eventManagementSystem.model.User;
-//import com.example.eventManagementSystem.service.UserService;
-//import jakarta.validation.Valid;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@CrossOrigin
-//public class UserController {
-//    @Autowired
-//    private UserService userService;
-//    private final Logger logger = LoggerFactory.getLogger(UserController.class);
-//    @PostMapping("/users")
-//    public User saveUser(@Valid @RequestBody User user){
-//        logger.info("Inside saveUser of UserController");
-//        return userService.saveUser(user);
-//    }
-//    @GetMapping("/users")
-//    public List<User> fetchUser(){
-//        logger.info("Inside fetchUser of UserController");
-//        return userService.fetchUser();
-//    }
-//    @GetMapping("/users/{id}")
-//    public User fetchUserById(@PathVariable("id") Long id){
-//        return userService.fetchUserById(id);
-//    }
-//    @DeleteMapping("/users/{id}")
-//    public String deleteUserById(@PathVariable("id") Long id){
-//        userService.deleteUserById(id);
-//        return "Deleted Successfully";
-//    }
-//    @PutMapping("/users/{id}")
-//    public User updateUserById(@PathVariable("id") Long id, @RequestBody User user){
-//        return userService.updateUserById(id,user);
-//    }
-//
-//
-//}
+package com.example.eventManagementSystem.controller;
+
+import com.example.eventManagementSystem.model.User;
+import com.example.eventManagementSystem.service.UserService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+    @PostMapping("/users")
+    public User saveUser(@Valid @RequestBody User user){
+        logger.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+    @GetMapping("/users")
+    public List<User> fetchUser(){
+        logger.info("Inside fetchUser of UserController");
+        return userService.fetchUser();
+    }
+    @GetMapping("/users/{id}")
+    public User fetchUserById(@PathVariable("id") Long id){
+        return userService.fetchUserById(id);
+    }
+    @DeleteMapping("/users/{id}")
+    public String deleteUserById(@PathVariable("id") Long id){
+        userService.deleteUserById(id);
+        return "Deleted Successfully";
+    }
+    @PutMapping("/users/{id}")
+    public User updateUserById(@PathVariable("id") Long id, @RequestBody User user){
+        return userService.updateUserById(id,user);
+    }
+
+
+}
--- eventManagementSystem/eventManagementSystem/src/main/java/com/example/eventManagementSystem/service/UserServiceImpl.java
+++ eventManagementSystem/eventManagementSystem/src/main/java/com/example/eventManagementSystem/service/UserServiceImpl.java
@@ -1,57 +1,57 @@
-//package com.example.eventManagementSystem.service;
-//
-//import com.example.eventManagementSystem.model.User;
-//import com.example.eventManagementSystem.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class UserServiceImpl implements UserService{
-//    @Autowired
-//    private UserRepository userRepository;
-//    @Override
-//    public User saveUser(User user) {
-//        return userRepository.save(user);
-//    }
-//
-//    @Override
-//    public List<User> fetchUser() {
-//        return userRepository.findAll();
-//    }
-//
-//    @Override
-//    public User fetchUserById(Long id) {
-//        return userRepository.findById(id).get();
-//    }
-//
-//    @Override
-//    public void deleteUserById(Long id) {
-//        userRepository.deleteById(id);
-//    }
-//
-//    @Override
-//    public User updateUserById(Long id, User user) {
-//        Optional<User> existingUserOptional = userRepository.findById(id);
-//        if(existingUserOptional.isPresent()) {
-//            User existingUser = existingUserOptional.get();
-//            if (user.getUsername() != null) {
-//                existingUser.setUsername(user.getUsername());
-//            }
-//            if (user.getPassword() != null) {
-//                existingUser.setPassword(user.getPassword());
-//            }
-//            if (user.getEmail() != null) {
-//                existingUser.setEmail(user.getEmail());
-//            }
-//            if (user.getRole() != null) {
-//                existingUser.setRole(user.getRole());
-//            }
-//            return userRepository.save(existingUser);
-//        } else {
-//            throw new RuntimeException("Registration not found with id" +id);
-//        }
-//    }
-//}
+package com.example.eventManagementSystem.service;
+
+import com.example.eventManagementSystem.model.User;
+import com.example.eventManagementSystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> fetchUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User fetchUserById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User updateUserById(Long id, User user) {
+        Optional<User> existingUserOptional = userRepository.findById(id);
+        if(existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+            if (user.getUsername() != null) {
+                existingUser.setUsername(user.getUsername());
+            }
+            if (user.getPassword() != null) {
+                existingUser.setPassword(user.getPassword());
+            }
+            if (user.getEmail() != null) {
+                existingUser.setEmail(user.getEmail());
+            }
+            if (user.getRole() != null) {
+                existingUser.setRole(user.getRole());
+            }
+            return userRepository.save(existingUser);
+        } else {
+            throw new RuntimeException("Registration not found with id" +id);
+        }
+    }
+}
